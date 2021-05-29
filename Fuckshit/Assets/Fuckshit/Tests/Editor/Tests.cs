d962eb2cad3a3471a2aebfedac398875e0944c3a
vis2k <info@noobtuts.com>
2021-05-29 20:17:29 +0800

    Tests, Scripts folder

---
+++ Fuckshit/Assets/Fuckshit/Tests/Editor/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Fuckshit.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            // TODO
+        }
+    }
+}
