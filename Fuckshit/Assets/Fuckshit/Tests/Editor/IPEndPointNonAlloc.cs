4c41cee4ca6c4d6be8604bf2ef15a7312cd851dd
vis2k <info@noobtuts.com>
2021-05-29 20:46:14 +0800

    ReceiveFrom with IPEndPointNonAlloc

--- Fuckshit/Assets/Fuckshit/Tests/Editor/IPEndPointNonAlloc.cs
+++ Fuckshit/Assets/Fuckshit/Tests/Editor/IPEndPointNonAlloc.cs
@@ -1,20 +1,32 @@
-﻿using System.Net;
+﻿using System;
+using System.Linq;
+using System.Net;
 using NUnit.Framework;
 
 namespace Fuckshit.Tests
 {
     public class IPEndPointNonAllocTests : UdpTest
     {
+        byte[] message = {0xAA, 0xBB};
+
         // simply try to create one
         [Test]
-        public void NewIPEndPointNonAlloc()
+        public void New()
         {
-            IPEndPointNonAlloc endPoint = new IPEndPointNonAlloc(IPAddress.Any, 1337);
+            IPEndPointNonAlloc _ = new IPEndPointNonAlloc(IPAddress.Any, 1337);
         }
 
         [Test]
-        public void SendToServer()
+        public void ReceiveFrom()
         {
+            // send a message to server
+            ClientSend(message);
+
+            // poll with IPEndPointNonAlloc
+            EndPoint newClientEP = new IPEndPointNonAlloc(IPAddress.Any, 0);
+            bool result = ServerPoll(ref newClientEP, out ArraySegment<byte> received);
+            Assert.That(result, Is.True);
+            Assert.That(received.SequenceEqual(message));
         }
     }
 }
