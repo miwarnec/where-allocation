e9212ba1409432fab1605ca8b92e754588d24cfa
vis2k <info@noobtuts.com>
2021-05-30 13:03:09 +0800

    add test

--- Fuckshit/Assets/Fuckshit/Tests/Editor/IPEndPointNonAlloc.cs
+++ Fuckshit/Assets/Fuckshit/Tests/Editor/IPEndPointNonAlloc.cs
@@ -8,6 +8,7 @@ namespace Fuckshit.Tests
     public class IPEndPointNonAllocTests : UdpTest
     {
         byte[] message = {0xAA, 0xBB};
+        byte[] message2 = {0xCC, 0xDD};
 
         // simply try to create one
         [Test]
@@ -27,6 +28,30 @@ namespace Fuckshit.Tests
             bool result = ServerPoll(ref newClientEP, out ArraySegment<byte> received);
             Assert.That(result, Is.True);
             Assert.That(received.SequenceEqual(message));
+        }
+
+        // send two messages to server
+        [Test]
+        public void ReceiveFromMultiple()
+        {
+            // send a message to server
+            ClientSend(message);
+
+            // poll with IPEndPointNonAlloc
+            EndPoint newClientEP = new IPEndPointNonAlloc(IPAddress.Any, 0);
+            bool result = ServerPoll(ref newClientEP, out ArraySegment<byte> received);
+            Assert.That(result, Is.True);
+            Assert.That(received.SequenceEqual(message));
+
+
+            // send two messages to server
+            ClientSend(message2);
+
+            // poll with IPEndPointNonAlloc
+            newClientEP = new IPEndPointNonAlloc(IPAddress.Any, 0);
+            result = ServerPoll(ref newClientEP, out received);
+            Assert.That(result, Is.True);
+            Assert.That(received.SequenceEqual(message2));
         }
     }
 }
