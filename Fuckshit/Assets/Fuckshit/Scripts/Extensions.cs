0abce703443feced4af99c017112526ebd35e9ec
vis2k <info@noobtuts.com>
2021-06-01 11:45:31 +0800

    fix: ReceiveFrom resets m_changed so that GetHashCode() is not always 0 for SocketAddress.

--- Fuckshit/Assets/Fuckshit/Scripts/Extensions.cs
+++ Fuckshit/Assets/Fuckshit/Scripts/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using System.Net.Sockets;
 
@@ -27,7 +28,35 @@ namespace Fuckshit
             // obvious that IPEndPointNonAlloc.Create does NOT create a new
             // IPEndPoint. it saves the result in IPEndPointNonAlloc.temp!
             EndPoint casted = remoteEndPoint;
-            return socket.ReceiveFrom(buffer, offset, size, socketFlags, ref casted);
+            int received = socket.ReceiveFrom(buffer, offset, size, socketFlags, ref casted);
+
+            // remoteEndPoint's SocketAddress has m_changed=false after
+            // ReceiveFrom, because ReceiveFrom seems to just copy into the buffer.
+            //
+            // GetHashCode() would return 0 with m_changed=false:
+            // https://github.com/mono/mono/blob/bdd772531d379b4e78593587d15113c37edd4a64/mcs/class/referencesource/System/net/System/Net/SocketAddress.cs#L262
+            //
+            // we need to set m_changed=true.
+            // the only way to do that is by _actually_ modifying the buffer:
+            // https://github.com/mono/mono/blob/bdd772531d379b4e78593587d15113c37edd4a64/mcs/class/referencesource/System/net/System/Net/SocketAddress.cs#L99
+            // so let's do that.
+            // -> unchecked in case it's byte.Max
+            if (remoteEndPoint.temp.GetHashCode() == 0)
+            {
+                unchecked
+                {
+                    remoteEndPoint.temp[0] += 1;
+                    remoteEndPoint.temp[0] -= 1;
+                }
+            }
+
+            // make sure this worked.
+            // at least throw an Exception to make it obvious if the trick does
+            // not work anymore, in case ReceiveFrom is ever changed.
+            if (remoteEndPoint.temp.GetHashCode() == 0)
+                throw new Exception($"SocketAddress GetHashCode() is 0 after ReceiveFrom. Does the m_changed trick not work anymore?");
+
+            return received;
         }
 
         // same as above, different parameters
